e966e00a61b6bc664c1375384ce13bc51b36a747
cemen <frodo.work@gmail.com>
2025-09-24 01:08:50 +0300

    added postgresql db and pgadmin containers into docker-compose

--- src/database/__init__.py
+++ src/database/__init__.py
@@ -1,1 +1,11 @@
+import os
+
 import models
+
+environment = os.getenv("ENVIRONMENT", "developing")
+
+# TODO: implement switching db to sqlite/postgresql depends on environment state (developing/testing)
+from database.session_postgresql import (
+    get_postgresql_db as get_db,
+    get_postgresql_db_contextmanager as get_db_contextmanager,
+)
